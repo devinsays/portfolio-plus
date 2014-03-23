23329837b0efdd3844884f8ac742b6ab74c6e03a
Devin Price <devin@wptheming.com>
2014-03-23 14:58:02 -0500

    Update page meta template correctly.

--- extensions/extras.php
+++ extensions/extras.php
@@ -112,7 +112,7 @@ function portfolioplus_update_page_templates() {
 					break;
 			}
 			if ( $new_template ) {
-				update_post_meta( get_the_ID(), '_wp_page_template', true );
+				update_post_meta( get_the_ID(), '_wp_page_template', $new_template );
 			}
 		endwhile;
 	endif;
