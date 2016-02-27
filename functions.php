a580107b44e7649cef6e25be6ab6d5f422b1283c
Devin Price <devin@wptheming.com>
2016-02-27 16:35:21 -0600

    Version 3.8.2

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * Set constant for version
  */
-define( 'PORTFOLIO_VERSION', '3.8.1' );
+define( 'PORTFOLIO_VERSION', '3.8.2' );
 
 /**
  * Set the content width based on the theme's design and stylesheet.
