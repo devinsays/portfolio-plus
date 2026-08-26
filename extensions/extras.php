d5abe39919e3a325c6c7e01560a0e45534b0953e
Devin Price <devin@wptheming.com>
2026-08-26 11:52:05 -0500

    Use title-tag theme support instead of wp_title().
    Fixes #25

--- extensions/extras.php
+++ extensions/extras.php
@@ -3,38 +3,6 @@
  * @package Portfolio+
  */
 
- /**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function portfolioplus_wp_title( $title, $sep ) {
-	if ( is_feed() ) {
-		return $title;
-	}
-
-	global $page, $paged;
-
-	// Add the blog name
-	$title .= get_bloginfo( 'name', 'display' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title .= " $sep $site_description";
-	}
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 ) {
-		$title .= " $sep " . sprintf( __( 'Page %s', 'portfolio-plus' ), max( $paged, $page ) );
-	}
-
-	return $title;
-}
-add_filter( 'wp_title', 'portfolioplus_wp_title', 10, 2 );
-
 /**
  * Displays notice if post_per_page is not divisible by 3
  */
